d897f8ce4f81f47710308b25fbf762fba4a4775b
Dave Vandan <vandandave18@gmail.com>
2021-04-23 06:13:41 -0400

    final

---
+++ evernote-clone/src/config/fireconfig.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase'
+import 'firebase/firestore';
+
+
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCLyvc9CQ1YrNY4Sb836TR0srSBIXqe32E",
+  authDomain: "evernote-clone-2d198.firebaseapp.com",
+  projectId: "evernote-clone-2d198",
+  storageBucket: "evernote-clone-2d198.appspot.com",
+  messagingSenderId: "910198642605",
+  appId: "1:910198642605:web:73776102367edb990eb324"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
+  
+
+  export {firebaseConfig};
---
+++ evernote-clone/src/sidebar/sidebar.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import styles from '../sidebar/style';
+import List from '@material-ui/core/List';
+import { Divider, Button } from '@material-ui/core';
+import SidebarItemComponent from '../sidebar-item/sidebar-item';
+
+
+class SidebarComponent extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        addingNote: false,
+        title: null
+      };
+    }
+    render(){
+
+      const {notes, classes, selectedNoteIndex } = this.props;
+
+       if(notes){
+        return(
+        
+          <div className = {classes.SidebarContainer}>
+            <Button
+            onClick = {this.newNoteBtnClick}
+            className = {classes.newNoteBtn}>
+              
+              {this.state.addingNote ? 'cancel' : 'new note'}
+            </Button>
+            {
+              this.state.addingNote ? 
+              <div>
+                <input type="text"
+                className = {classes.newNoteInput}
+                placeholder="enter note title"
+                onKeyUp={(e) => this.updateTitle(e.target.value)}>
+               </input>
+               <Button className = {classes.newNoteSubmitBtn}
+               onClick={this.newNote}>
+                 submit note
+               </Button>
+              </div> :
+              null
+            }
+            <List>
+              {
+                  notes.map((_note, _index) => {
+                    return(
+                      <div key={_index}>
+                          <SidebarItemComponent
+                          _note = {_note}
+                          _index = {_index}
+                          selectedNoteIndex = {selectedNoteIndex}
+                          selectNote={this.selectNote}
+                          deleteNote={this.deleteNote}>
+  
+                          </SidebarItemComponent>
+                          <Divider></Divider>
+  
+                          
+                      </div>
+                    )
+                  })
+  
+              }
+            </List>
+  
+  
+          </div>
+          
+          );
+       } else {
+         return(<div>add a note</div>)
+       }
+    }
+
+    newNoteBtnClick = () => {
+       this.setState({ addingNote: !this.state.addingNote});
+     
+    }
+    updateTitle = (txt) => {
+      this.setState({title: txt})
+
+    }
+
+    newNote = () => {
+
+    this.props.newNote(this.state.title)
+    this.setState({title: null, addingNote: false})
+    }
+
+    selectNote = (n, i ) => this.props.selectNote(n, i);
+
+    deleteNote = (note) =>{
+        this.props.deleteNote(note)
+    }
+
+    }
+    
+    export default withStyles(styles)(SidebarComponent);
